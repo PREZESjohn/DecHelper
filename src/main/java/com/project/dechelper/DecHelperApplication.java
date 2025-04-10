46b734f0cc948acb30548b37455327e92189d31d
PREZESjohn <103060257+PREZESjohn@users.noreply.github.com>
2025-04-10 11:07:36 +0200

    Initial commit

---
+++ src/main/java/com/project/dechelper/DecHelperApplication.java
@@ -0,0 +1,13 @@
+package com.project.dechelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DecHelperApplication.class, args);
+    }
+
+}
