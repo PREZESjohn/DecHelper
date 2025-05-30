00a640b9f837ded043ccf8160b5cf9279cdbfae8
PREZESjohn <103060257+PREZESjohn@users.noreply.github.com>
2025-05-30 17:52:49 +0200

    add Spring AI RAG for AI reasoning

--- src/main/java/com/project/dechelper/controllers/ChatController.java
+++ src/main/java/com/project/dechelper/controllers/ChatController.java
@@ -5,8 +5,12 @@ import com.project.dechelper.model.SentenceDTO;
 import com.project.dechelper.services.AiSearchService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -20,69 +24,23 @@ import java.util.stream.Collectors;
 public class ChatController {
     private final ChatClient chatClient;
     private final ChatMemory chatMemory;
-    private final AiSearchService aiSearchService;
+    @Autowired
+    VectorStore vectorStore;
 
-    public ChatController(ChatClient chatClient, ChatMemory chatMemory, AiSearchService aiSearchService) {
+    public ChatController(ChatClient chatClient, ChatMemory chatMemory) {
         this.chatClient = chatClient;
         this.chatMemory = chatMemory;
-        this.aiSearchService = aiSearchService;
     }
 
-    @GetMapping("/generate")
-    public ResponseEntity<String> generate(@RequestParam(value = "message") String message){
-        try {
-            List<Information> relevantData = aiSearchService.getRelevantData(SentenceDTO.builder().sentence(message).returnInfoAmount(2).build());
-            String relevantDataString = relevantData.stream().map(Information::getContent).collect(Collectors.joining("\n"));
-            String combinedMessage = """
-                    %s
-                    
-                    Context information is below.
-                    
-                    ---------------------
-                    %s
-                    ---------------------
-                    
-                    Given the context information, answer the query.
-                    
-                    Follow these rules:
-                    
-                    1. If the answer is not in the context, just say that you don't know.
-                    2. Avoid statements like "Based on the context..." or "The provided information...".
-                    """.formatted(message, relevantDataString);
-            String response = chatClient.prompt()
-                    .user(combinedMessage)
-                    .call()
-                    .content()
-                    .toString();
-            return ResponseEntity.ok(response);
-        }catch (Exception e){
-            e.printStackTrace();
-            return ResponseEntity.internalServerError().body("Something went wrong");
-        }
-    }
     @GetMapping("/stream")
     public ResponseEntity<Flux<String>> generateStream(@RequestParam(value = "message") String message){
         try {
-            List<Information> relevantData = aiSearchService.getRelevantData(SentenceDTO.builder().sentence(message).returnInfoAmount(2).build());
-            String relevantDataString = relevantData.stream().map(Information::getContent).collect(Collectors.joining("\n"));
-            String combinedMessage = """
-                    %s
-                    
-                    Context information is below.
-                    
-                    ---------------------
-                    %s
-                    ---------------------
-                    
-                    Given the context information, answer the query.
-                    
-                    Follow these rules:
-                    
-                    1. If the answer is not in the context, just say that you don't know.
-                    2. Avoid statements like "Based on the context..." or "The provided information...".
-                    """.formatted(message, relevantDataString);
+            QuestionAnswerAdvisor qa=QuestionAnswerAdvisor.builder(vectorStore)
+                    .searchRequest(SearchRequest.builder().similarityThreshold(0.4d).build()).build();
+
             Flux<String> response = chatClient.prompt()
-                    .user(combinedMessage)
+                    .advisors(qa)
+                    .user(message)
                     .stream()
                     .content();
             return ResponseEntity.ok(response);
@@ -95,6 +53,7 @@ public class ChatController {
     public List<Message> getHistory(){
         return chatMemory.get("default", 20);
     }
+
     @DeleteMapping("history")
     public ResponseEntity<String> deleteHistory(){
         chatMemory.clear("default");
