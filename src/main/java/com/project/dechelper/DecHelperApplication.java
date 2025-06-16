bb9f11abf6911a7d60ecde9bc2033bd8f7191ba2
PREZESjohn <103060257+PREZESjohn@users.noreply.github.com>
2025-06-16 17:29:09 +0200

    using toolCalling for RAG

--- src/main/java/com/project/dechelper/DecHelperApplication.java
+++ src/main/java/com/project/dechelper/DecHelperApplication.java
@@ -1,6 +1,7 @@
 package com.project.dechelper;
 
 import com.project.dechelper.toolCalling.DataRetrivalTools;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,13 +9,14 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class DecHelperApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(DecHelperApplication.class, args);
     }
 
     @Bean
-    public DataRetrivalTools dataRetrivalTools() {
-        return new DataRetrivalTools();
+    public DataRetrivalTools dataRetrivalTools(VectorStore vectorStore) {
+        return new DataRetrivalTools(vectorStore);
     }
 
 }
--- src/main/java/com/project/dechelper/toolCalling/DataRetrivalTools.java
+++ src/main/java/com/project/dechelper/toolCalling/DataRetrivalTools.java
@@ -2,6 +2,7 @@ package com.project.dechelper.toolCalling;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.rag.Query;
 import org.springframework.ai.rag.retrieval.search.DocumentRetriever;
@@ -17,25 +18,26 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@RequiredArgsConstructor
 public class DataRetrivalTools {
 
+    private final VectorStore vectorStore;
+
+    @Tool(description = "Get data from RAG about informations from user prompt")
+    public String getRelevantData(String query){
+        DocumentRetriever retriever = VectorStoreDocumentRetriever.builder()
+                .vectorStore(vectorStore)
+                .similarityThreshold(0.55)
+                .topK(20)
+                .build();
+        List<Document> documents = retriever.retrieve(new Query(query));
+        String documentsString = documents.stream().map(Document::getText).collect(Collectors.joining("\n"));
+        System.out.println("Documents from RAG: "+documentsString);
+        return documentsString;
+    }
     @Tool(description = "Current date and time in the user's timezone")
     public String getCurrentDateTime(){
         System.out.println("------------uruchomienie toola---------------");
         return LocalDateTime.now().atZone(LocaleContextHolder.getTimeZone().toZoneId()).toString();
     }
-    //    private final VectorStore vectorStore;
-//
-//    @Tool(description = "Get data from RAG about informations from user prompt")
-//    public String getRelevantData(String query){
-//        DocumentRetriever retriever = VectorStoreDocumentRetriever.builder()
-//                .vectorStore(vectorStore)
-//                .similarityThreshold(0.55)
-//                .topK(20)
-//                .build();
-//        List<Document> documents = retriever.retrieve(new Query(query));
-//        String documentsString = documents.stream().map(Document::getFormattedContent).collect(Collectors.joining("\n"));
-//        System.out.println("Documents from RAG: "+documents);
-//        return documentsString;
-//    }
 }
