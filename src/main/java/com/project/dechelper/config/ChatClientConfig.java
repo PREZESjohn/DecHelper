5ea9095c32a235912adff9ba8fad334a484c7724
PREZESjohn <103060257+PREZESjohn@users.noreply.github.com>
2025-07-05 12:10:29 +0200

    fix chat system configuration message

--- src/main/java/com/project/dechelper/config/ChatClientConfig.java
+++ src/main/java/com/project/dechelper/config/ChatClientConfig.java
@@ -1,21 +1,29 @@
 package com.project.dechelper.config;
 
+import lombok.SneakyThrows;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class ChatClientConfig {
 
+    @Value("classpath:chatSystemConfig.txt")
+    private Resource chatSystemConfig;
 
+    @SneakyThrows
     @Bean
     public ChatClient chatClient(ChatClient.Builder builder, ChatMemory chatMemory) {
         return builder
                 .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
-                .defaultSystem("You are helpful chat bot that answers questions based on data retrieved from RAG system. You have in disposal two sets of tools: one for data retrieval from RAG and other current informations, second one for adding, modifing, deleteting data in Vector Store. Answer questions by using tools.")
+                .defaultSystem(chatSystemConfig.getContentAsString(StandardCharsets.UTF_8))
                 //.defaultOptions(ChatOptions.builder().temperature(0d).build())
                 //ustawienie temperatury na 0 wylacza wywolywanie tooli przez model
                 .build();
