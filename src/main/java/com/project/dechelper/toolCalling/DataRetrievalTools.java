ae97860462fee272485b910abe0969455ce491a9
PREZESjohn <103060257+PREZESjohn@users.noreply.github.com>
2025-08-13 17:42:54 +0200

    feature: tool for searching RAG by time range

--- src/main/java/com/project/dechelper/toolCalling/DataRetrievalTools.java
+++ src/main/java/com/project/dechelper/toolCalling/DataRetrievalTools.java
@@ -8,6 +8,7 @@ import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
 import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.ai.tool.annotation.ToolParam;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.filter.FilterExpressionBuilder;
 import org.springframework.context.i18n.LocaleContextHolder;
 
 import java.time.LocalDateTime;
@@ -33,6 +34,30 @@ public class DataRetrievalTools {
         log.info("Query: "+query+" | Documents from RAG: "+documentsString);
         return documentsString;
     }
+
+    @Tool(description = "Get data from specific time range")
+    public String getDataByTimeRange(
+            @ToolParam(description = "Subject of user prompt") String query,
+            @ToolParam(description = "Start date of search") String startDate,
+            @ToolParam(description = "End date of search") String endDate){
+
+        FilterExpressionBuilder b = new FilterExpressionBuilder();
+
+        DocumentRetriever retriever = VectorStoreDocumentRetriever.builder()
+                .vectorStore(vectorStore)
+                .similarityThreshold(0.55)
+                .filterExpression(b.and(
+                        b.gte("createdOn",startDate),
+                        b.lte("createdOn",endDate)
+                ).build())
+                .topK(20)
+                .build();
+        List<Document> documents = retriever.retrieve(new Query(query));
+        String documentsString = documents.stream().map(Document::getText).collect(Collectors.joining("\n"));
+        log.info("Query: "+query+" | Time range: "+startDate+" - "+endDate+" | Documents from RAG: "+documentsString);
+        return documentsString;
+    }
+
     @Tool(description = "Current date and time in the user's timezone")
     public String getCurrentDateTime(){
         log.info("Date and time: "+LocalDateTime.now());
